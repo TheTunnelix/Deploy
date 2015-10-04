20f41f4430a0568553d24c918e01aa3b879f33d3
TheTunnelix <tunnelixpower@gmail.com>
2015-10-04 13:06:14 +0400

    Update Deploy-wordpress-with-Apache.sh

--- Deploy-wordpress-with-Apache.sh
+++ Deploy-wordpress-with-Apache.sh
@@ -2,40 +2,39 @@
 # Author -TheTunnelix
 
 #Update the CentOS after minimum install
-update=`yum update -y`
+#update=`yum update -y`
 
 #Install repo and do a clean all
-repo=`yum install epel-release -y && yum clean all
-`
+#repo=`yum install epel-release -y && yum clean all`
+
 #Install prerequisite tools packages for troubleshooting
-tools=`yum install nmap traceroute tcpdump iftop htop w3m vim wget -y`
+#tools=`yum install nmap traceroute tcpdump iftop htop w3m vim wget -y`
 
 #Install the php,mysql,httpd with their prerequisites
-InstallationOfApp=`yum install php php-gd php-mysql mysql-server mysql httpd httpd-devel -y`
+#InstallationOfApp=`yum install php php-gd php-mysql mysql-server mysql httpd httpd-devel -y`
 
 #Download wordpress now for installation
-DownloadWordpress=`wget http://wordpress.org/latest.tar.gz -P /tmp`
+#DownloadWordpress=`wget http://wordpress.org/latest.tar.gz -P /tmp`
 
 #Chmodlastest.tar.gz
-chmodwordpress=`chmod + /tmp/latest.tar.gz`
+#chmodwordpress=`chmod + /tmp/latest.tar.gz`
 
 #UntarInto/var/www
-untarWordpress=`tar -xvzf /tmp/latest.tar.gz -C /var/www`
+#untarWordpress=`tar -xvzf /tmp/latest.tar.gz -C /var/www`
 
 #ChownDocumentRootwordpress
-ChownWordpress=`chown -R apache:apache /var/www/wordpress`
+#ChownWordpress=`chown -R apache:apache /var/www/wordpress`
 
 #CreatingTheVhost
-CreatingTheVhost=
-`printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n'
-'<VirtualHost *:80>' 
-'DocumentRoot /var/www/wordpress' 
-'ServerName www.myweb.com'
-'ServerAlias myweb.com'
-'<Directory /var/www/wordpress>'
-'Options FollowSymlinks'
-'Allow from all'
-'</Directory'
-'ErrorLog /var/log/httpd/wordpress-error-log'
-'CustomLog /var/log/httpd/wordpress-acess-log common'
-'</VirtualHost' >> /etc/httpd/conf.d/myweb.conf`
+
+`OpenVirtualHost= printf '%s\n' '<VirtualHost *:80>' >> /etc/httpd/conf.d/myweb.conf`
+`InsertDocumentRoot= printf '%s\n' 'DocumentRoot /var/www/wordpress'` >> /etc/httpd/conf.d/myweb.conf
+#'ServerName www.myweb.com'
+#'ServerAlias myweb.com'
+#'<Directory /var/www/wordpress>'
+#'Options FollowSymlinks'
+#'Allow from all'
+#'</Directory'
+#'ErrorLog /var/log/httpd/wordpress-error-log'
+#'CustomLog /var/log/httpd/wordpress-acess-log common'
+#'</VirtualHost' >>?> /etc/httpd/conf.d/myweb.conf
