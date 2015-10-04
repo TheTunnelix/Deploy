c3623ad251ebdf9a7c0fc6a63b82b9c37fd4a9b8
TheTunnelix <tunnelixpower@gmail.com>
2015-10-04 11:44:40 +0400

    Update Deploy-wordpress-with-Apache.sh

--- Deploy-wordpress-with-Apache.sh
+++ Deploy-wordpress-with-Apache.sh
@@ -1,1 +1,15 @@
-~
+#! /bin/bash
+# Author -TheTunnelix
+
+#Update the CentOS after minimum install
+update=`yum update -y`
+
+#Install repo and do a clean all
+repo=`yum install epel-release -y && yum clean all
+`
+#Install prerequisite tools packages for troubleshooting
+tools=`yum install nmap traceroute tcpdump iftop htop w3m vim -y`
+
+#Install the php,mysql,httpd with their prerequisites
+installA=`yum install php php-gd php-mysql mysql-server mysql -y`
+
