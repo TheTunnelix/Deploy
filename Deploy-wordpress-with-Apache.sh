5407ab22fe21fb082a97593e0217b824824a2873
TheTunnelix <tunnelixpower@gmail.com>
2015-10-04 13:27:59 +0400

    Update Deploy-wordpress-with-Apache.sh

--- Deploy-wordpress-with-Apache.sh
+++ Deploy-wordpress-with-Apache.sh
@@ -31,10 +31,11 @@
 `InsertDocumentRoot=  printf '%s\n'    'DocumentRoot /var/www/wordpress'   >> /etc/httpd/conf.d/myweb.conf`
 `InsertServerName=    printf '%s\n'    'ServerName www.myweb.com'          >> /etc/httpd/conf.d/myweb.conf`
 `InsertServerAlias=   printf '%s\n'    'ServerAlias myweb.com'             >> /etc/httpd/conf.d/myweb.conf`
-`OpenDirectory=       printf '%s\n'    '<Directory /var/www/wordpress>'    >> /etc/httpd/conf.d/myweb.conf`
-`InsertOptionsfsym=   printf '%s\n'    'Options FollowSymlinks'            >> /etc/httpd/conf.d/myweb.conf`
-`InsertAllow=         printf '%s\n'    'Allow from all'                    >> /etc/httpd/conf.d/myweb.conf`
-`CloseDirectory=      printf '%s\n'    '</Directory'                       >> /etc/httpd/conf.d/myweb.conf`
-`InsertErrorLog=      printf '%s\n'    '/var/log/httpd/error-log'          >> /etc/httpd/conf.d/myweb.conf`
-`InsertAcessLog=      printf '%s\n'    '/var/log/httpd/access-log common'  >> /etc/httpd/conf.d/myweb.conf`
-`CloseVirtualHost=    printf '%s\n'    '</VirtualHost'                     >> /etc/httpd/conf.d/myweb.conf`
+`OpenDirectory=       printf '%s\n'    '<Directory /var/www/wordpress>'              >> /etc/httpd/conf.d/myweb.conf`
+`InsertOptionsfsym=   printf '%s\n'    'Options FollowSymlinks'                      >> /etc/httpd/conf.d/myweb.conf`
+`InsertAllow=         printf '%s\n'    'Allow from all'                              >> /etc/httpd/conf.d/myweb.conf`
+`CloseDirectory=      printf '%s\n'    '</Directory'                                 >> /etc/httpd/conf.d/myweb.conf`
+`InsertErrorLog=      printf '%s\n'    'ErrorLog /var/log/httpd/error-log'           >> /etc/httpd/conf.d/myweb.conf`
+`InsertAcessLog=      printf '%s\n'    'CustomLog /var/log/httpd/access-log common'  >> /etc/httpd/conf.d/myweb.conf`
+`CloseVirtualHost=    printf '%s\n'    '</VirtualHost'                               >> /etc/httpd/conf.d/myweb.conf`
+
